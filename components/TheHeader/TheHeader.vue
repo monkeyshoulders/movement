41119f732e104cae600103e07c081adec11045a7
Matt Bledsoe <mattbledsoe@Matts-MacBook-Pro.local>
2018-04-16 15:59:10 -0400

    header work

--- components/TheHeader/TheHeader.vue
+++ components/TheHeader/TheHeader.vue
@@ -2,7 +2,7 @@
 <template>
   <header class="main-header">
     <nav class="main-nav">
-      <nuxt-link to="/"><a class="logo"><img src="../assets/logo.png" alt=""></a></nuxt-link>
+      <nuxt-link to="/"><a ><img class="logo" src="../../assets/logo.png" alt=""></a></nuxt-link>
       <!-- <nuxt-link to="/"><a><i class="fa fa-bars fa-lg"></i></a></nuxt-link> -->
     </nav>
   </header>
@@ -21,10 +21,17 @@ export default {
   top: 0;
   left: 0;
   width: 100%;
+  height: 70px;
 
 }
 
 .main-nav {
+  display: flex;
+}
 
+.logo {
+  width: 50px;
+  height: auto;
+  margin: 30px 0 20px 30px;
 }
 </style>
