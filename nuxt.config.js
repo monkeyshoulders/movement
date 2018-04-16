01c37ea477136a857bebc684669ab83450821e3e
Matt Bledsoe <mattbledsoe@Matts-MacBook-Pro.local>
2018-04-16 14:51:42 -0400

    added fonts and removed a readme

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,14 +3,15 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'movement',
+    title: 'Movement Landing Page',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'movement landing page' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:400,700,800|Playfair+Display:900' }
     ]
   },
   modules: [
